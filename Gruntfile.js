a0ca9bc019208ee3be2f5ac8e2f9bd50a509b926
Glen Mailer <glen.mailer@bskyb.com>
2013-11-27 09:55:17 +0000

    Markdown building and def shef 2 problem page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var hljs = require('highlight.js');
+
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -12,6 +14,10 @@ module.exports = function(grunt) {
             sass: {
                 files: ['sass/**/*.{scss,sass}'],
                 tasks: ['sass:dist', 'notify:sass']
+            },
+            markdown: {
+                files: ['markdown/*.md'],
+                tasks: ['markdown:build', 'notify:markdown']
             }
         },
 
@@ -19,6 +25,11 @@ module.exports = function(grunt) {
             sass: {
                 options: {
                     message: 'Sass Complete'
+                }
+            },
+            markdown: {
+                options: {
+                    message: 'Markdown Complete'
                 }
             }
         },
@@ -29,13 +40,51 @@ module.exports = function(grunt) {
                     'css/main.css': 'sass/main.scss'
                 }
             }
+        },
+
+        markdown: {
+            build: {
+                files: [
+                    { src: 'markdown/meeting-2.md', dest: './meeting-2.html' }
+                ],
+                options: {
+                    template: 'markdown-template.html',
+                    markdownOptions: {
+                        gfm: true,
+                        highlight: function(code, lang) {
+                            var out = code;
+                            try {
+                                out = hljs.highlight(lang, code).value;
+                            } catch(e) {
+                                out = hljs.highlightAuto(code).value;
+                            }
+                            return out;
+                        }
+                    },
+                    preCompile: function(src, context) {
+                        // Extract title
+                        var m = /\# (.*?)\n/.exec(src);
+                        context.title = m && m[1].trim();
+
+                        // Real markup for <tag>
+                        src = src
+                            .replace(/<tag>/g, '<span class="tag">')
+                            .replace(/<\/tag>/g, '</span>')
+
+                        return src;
+                    }
+                }
+            }
         }
     })
 
-    grunt.registerTask('default', ['sass:dist', 'devserver', 'watch']);
+    grunt.registerTask('default', [
+        'sass:dist', 'markdown:build', 'devserver', 'watch'
+    ]);
 
     grunt.loadNpmTasks('grunt-devserver')
     grunt.loadNpmTasks('grunt-notify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-markdown');
 }
---
+++ markdown/meeting-2.md
@@ -0,0 +1,36 @@
+# Functional Programming Problems
+
+Blah blah problems, Haskell clojure blah blah.
+
+In order to get the most from these exercises, I recommend avoiding the helper functions in the core library of your chosen language and using the functional programming primitives I've <tag>tagged</tag> each problem with.
+
+## Warmup
+
+1. Get the nth element in a list where the first item is '1'
+    ```clojure
+    (get-element [ 5 6 7 8 ] 3)
+    ;=> 7
+    ```
+    <tag>Recursion</tag>
+
+2. Reverse a list
+    ```clojure
+    (reverse-list [ 1 2 3 4 5 ])
+    ;=> [ 5 4 3 2 1 ]
+    ```
+    <tag>Recursion</tag>
+
+3. Calculate the sum of the squares of all odd numbers in a list
+    ```clojure
+    (odd-square-sum [ 4 5 6 7 8 9 ])
+    ;=> 155
+    ```
+    <tag>Filter</tag> <tag>Map</tag> <tag>Reduce</tag>
+
+## Beginner
+
+
+## Experienced
+
+
+## Expert
